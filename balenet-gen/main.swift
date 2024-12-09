1ec4dc690ff1c87a1662db2ee4d738b854e1cec8
Francesco Balestrieri <bale@balenet.com>
2024-12-09 08:52:40 +0200

    Add some documentaiton comments

--- balenet-gen/main.swift
+++ balenet-gen/main.swift
@@ -5,6 +5,36 @@
 //  Created by Francesco Balestrieri on 8.12.2024.
 //
 
+// Generate the Balenet (www.balenet.com) website from markdown sources.
+//
+// The input directory should be structured as follows:
+//
+// /content/
+// |
+// +- post-1/some-post.md
+// +- post-2/some-other-post.md
+// ...
+//
+// If there are multiple .md files only the first one will be processed.
+//
+// The generated HTML will be:
+//
+// /public/
+// |
+// +- index.html
+// +- post-1/index.html
+// +- post-2/index.html
+// ...
+// A post's markdown should include a front matter in this format:
+//
+// +++
+// +++
+// title = "<post title>"
+// date = "YYYY-MM-DD"
+// +++
+// <post content>
+//
+
 import Foundation
 
 let contentPath = "/Users/baleboy/websites/balenet-gen/content"
