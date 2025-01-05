ce3233ecb6b77dd5b96c886ced08dc02547421ca
baleboy <bale@balenet.com>
2025-01-05 09:45:16 +0100

    Only embed youtube links if they include the #embed tag

--- balenet-gen/StaticSite.swift
+++ balenet-gen/StaticSite.swift
@@ -197,7 +197,11 @@ struct StaticSite {
 extension Modifier {
     static func youtubeEmbed() -> Modifier {
         return Modifier(target: .links) { html, markdown in
-            // Match either youtube.com/watch?v=ID or youtu.be/ID
+            // Only process links that end with #embed
+            guard html.contains("#embed") else {
+                return html
+            }
+            
             let pattern = "(?:youtube\\.com\\/watch\\?v=|youtu\\.be\\/)([a-zA-Z0-9_-]+)"
             
             guard let regex = try? NSRegularExpression(pattern: pattern, options: []),
